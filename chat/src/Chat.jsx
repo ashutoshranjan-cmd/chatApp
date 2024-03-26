4b27c75fc5a51ced933f152ecde2e26def52e761
Ashutosh Ranjan <72221793+ashutoshranjan-cmd@users.noreply.github.com>
2024-03-26 07:50:38 +0530

    Update Chat.jsx

--- chat/src/Chat.jsx
+++ chat/src/Chat.jsx
@@ -7,7 +7,7 @@ import Message from './Message/Message'
 import cross from '../src/closeIcon.png'
 import ReactScrolllToBottom from 'react-scroll-to-bottom'
 import './chat.css'
-const ENDPOINT = "https://chat-app-api-rust.vercel.app/"
+const ENDPOINT = "https://chat-app-api-theta.vercel.app/"
 let socket;
 const Chat = () => {
     const [messageData, setMessageData] = useState("");
